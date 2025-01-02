c005c06443f8ff50938535b89a004a6464afb625
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-01-02 18:40:00 -0300

    got all together in v0 graph

---
+++ jarvis.py
@@ -0,0 +1,30 @@
+from langgraph.prebuilt import tools_condition, ToolNode
+from langgraph.graph import START, StateGraph, MessagesState
+from langchain_core.messages import HumanMessage
+from agent.nodes import assistant
+from agent.tools import TOOLS
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
+config = {"configurable": {"thread_id": '1'}}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(TOOLS))
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges("assistant", tools_condition)
+builder.add_edge("tools", "assistant")
+
+# Compile graph
+graph = builder.compile(checkpointer=memory)
+
+if __name__ == "__main__":
+    messages = [HumanMessage(
+        content="Adicione 1 dia de tempo gasto na issue 44.")
+    ]
+    result = graph.invoke({"messages": messages}, config)
+
+    for message in result["messages"]:
+        print(message.content)
+        print("--" * 50)
